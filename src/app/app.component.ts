ca7a3e2915c5365a25242e07b308608a104c57ba
kirillchelikov <kirill.chelikov@yandex.ru>
2020-11-05 15:40:16 +0500

    add code reformation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Car} from './interfaces/Car';
-import { HttpRequests} from './http-requests/http-request';
+import {Component, OnInit} from '@angular/core';
+import {Car} from './interfaces/Car';
+import {HttpRequests} from './http-requests/http-request';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +11,10 @@ export class AppComponent implements OnInit {
   title = 'mashinki';
   cars: Car[];
   currentCar: Car = null;
+
   constructor(private http: HttpRequests) {
   }
+
   async ngOnInit(): Promise<void> {
     this.cars = await this.http.getCarData();
   }
